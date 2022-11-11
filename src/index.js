01bbc1ee7c0bbda45c7e33263834aa9611032035
mohanad badawy <mohanad20129876@gmail.com>
2022-11-11 22:58:47 +0200

    make it better

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <HashRouter>
       <Routes>
         <Route path="*" element={<App />}></Route>
       </Routes>
     </HashRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
